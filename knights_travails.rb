8cdb806a448b5e96f062c0971a175a6c607e139f
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-01-24 21:48:24 -0300

    feat: find number of moves to reach destiny
    The #path_search method can find the number of moves it takes to reach desired
    destination, but since the possible paths of each step are being reduced it can
    not keep track of the path taken to reach the destination.
    
    The 'origin' included as last element of the array returned by #find_moves shall
    provide the reference to this path on recursive calls, but to do so the queue
    passed as argument to the next call can't be reduced: the recursive calls should
    somehow be able to keep track of the path being taken to destiny.

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,29 +1,34 @@
-board = (1..8).to_a.repeated_permutation(2).to_a
+# frozen_string_literal: true
 
 def find_moves(origin)
   board = (1..8).to_a.repeated_permutation(2).to_a
-  [[origin[0] + 2, origin[1] + 1],
-  [origin[0] + 2, origin[1] - 1],
-  [origin[0] - 2, origin[1] + 1],
-  [origin[0] - 2, origin[1] - 1],
-  [origin[0] + 1, origin[1] + 2],
-  [origin[0] + 1, origin[1] - 2],
-  [origin[0] - 1, origin[1] + 2],
-  [origin[0] - 1, origin[1] - 2]].
-  intersection(board)
+  [
+    [origin[0] + 2, origin[1] + 1],
+    [origin[0] + 2, origin[1] - 1],
+    [origin[0] - 2, origin[1] + 1],
+    [origin[0] - 2, origin[1] - 1],
+    [origin[0] + 1, origin[1] + 2],
+    [origin[0] + 1, origin[1] - 2],
+    [origin[0] - 1, origin[1] + 2],
+    [origin[0] - 1, origin[1] - 2],
+    origin # to be referenced by the #path_search recursive call
+  ].intersection(board)
 end
 
-def path_search(origin, destiny, edges = 0, queue = [])
-  return edges += 1 if find_moves(origin).one?(destiny)
-
-  queue = find_moves(origin)
+def path_search(origin, destiny, edges = 1, queue = find_moves(origin),
+                path = '')
+  # iteration through the first tree level. On each of them add the possible
+  # moves from that position to the subsequent ones
+  path += origin.to_s
   queue.size.times do
-    queue <<
+    return p edges, path if queue[0] == destiny
+
+    queue << find_moves(queue[0])
+    queue.delete_at(0)
   end
+  path_search(origin, destiny, edges + 1, queue.reduce(:+), path)
 end
 
-def knight_moves(origin, destiny)
+def knight_moves(origin, destiny) end
 
-end
-
-p path_search([0,0],[1,3])
+path_search([0, 0], [3, 8])
